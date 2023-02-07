4943142b2344a466dc9ee294699b7d773a04fd39
FredyJabe <fredypimp@gmail.com>
2023-02-06 23:27:06 -0500

    Tried to fix stuff with the 5 minutes event

--- Every5Minutes.cs
+++ Every5Minutes.cs
@@ -22,7 +22,6 @@ public class CPHInline
 
             // If the user exists, read his points and update
             int points = (File.Exists(userFile)) ? Int32.Parse(File.ReadAllLines(userFile)[0]) + 5 : 5;
-            
 
             // And saves the file again
             using (StreamWriter writer = new StreamWriter(userFile)) {
