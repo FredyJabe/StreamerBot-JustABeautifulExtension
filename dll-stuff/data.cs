5d67ea2ff8653a25da9b18bfeaae76525722a93b
FredyJabe <fredypimp@gmail.com>
2023-02-23 15:28:22 -0500

    Data and Settings stuff

--- dll-stuff/data.cs
+++ dll-stuff/data.cs
@@ -4,17 +4,32 @@ using System.Data.SQLite;
 
 namespace JabeDLL {
     public static class Data {
-        private static string connection = "";
+        private static string connectionString = "";
+        private static SQLiteConnection connection;
 
-        public static object[] Request(string request) {
+        public static void Initialize() {
+            connectionString = @"Data Source=test.db;Version=3;New=True;Compress=True;";
+            connection = new SQLiteConnection(connectionString);
+        }
+
+        public static object[] Read(string request) {
             // TODO sqlite read data
             object[] retVal = {};
+            connection.Open();
 
+            
+
+            connection.Close();
             return retVal;
         }
 
         public static void Write(string request) {
             // TODO sqlite write data
+            connection.Open();
+
+
+
+            connectionString.Close();
         }
     } 
 }
--- dll-stuff/settings.cs
+++ dll-stuff/settings.cs
@@ -2,6 +2,42 @@ using System;
 
 namespace JabeDLL {
     public static class Settings {
-        // TODO settings stuff
+        private static string _httpHandlerUrl = @"http://127.0.0.1:7474/DoAction";
+        private static string _actionId = "";
+        private static string _actionName = "";
+        private static string _pathLOG = @"D:\Stream\Logs\" + DateTime.Now.ToShortDateString() + ".log";
+        private static string _pathTXT = @"D:\Stream\Commands\";
+        private static string _pathSFX = @"D:\Stream\Sounds\";
+        private static string _pathGFX = @"D:\Stream\Gifs\";
+        private static string _pathDATA = @"D:\Stream\Data\";
+
+        public static string HttpHandlerUrl {
+            get => _httpHandlerUrl;
+        }
+        public static string ActionId {
+            get => _actionId;
+        }
+        public static string actionName {
+            get => _actionName;
+        }
+        public static string PathLog {
+            get => _pathLOG;
+        }
+        public static string PathTxt {
+            get => _pathTXT;
+        }
+        public static string PathSfx {
+            get => _pathSFX;
+        }
+        public static string PathGfx {
+            get => _pathGFX;
+        }
+        public static string PathData {
+            get => _pathDATA;
+        }
+
+        public static void Initialize() {
+            string settingsPath = @"JabeDLL/";
+        }
     }
 }
