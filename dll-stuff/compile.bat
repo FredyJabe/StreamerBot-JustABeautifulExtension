61a5444f86ba740b8b919f0a81ac848d534b4684
Frederic "FredyJabe" Jackson-Beaudoin <fredypimp@gmail.com>
2023-08-29 12:05:59 -0400

    Delete dll-stuff directory

--- dll-stuff/compile.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe /r:../References/TagLibSharp.dll,../References/System.Data.SQLite.dll,../References/obs-websocket-dotnet.dll /target:library /out:./output/JabeDll.dll *.cs
