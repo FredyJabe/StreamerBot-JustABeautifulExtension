6c5c31d238a019749359cd0ed34a872ec70c2460
Frederic "FredyJabe" Jackson-Beaudoin <fredypimp@gmail.com>
2023-09-30 04:43:21 -0400

    Update streamStarted.cs
    Added a couple new variables

--- streamStarted.cs
+++ streamStarted.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 
 public class CPHInline
 {
@@ -14,17 +15,21 @@ public class CPHInline
 		CPH.SetGlobalVar("pathData", @"JabeChatCommands\Data\");
 		CPH.SetGlobalVar("pathView", @"JabeChatCommands\Data\Viewers\");
 		CPH.SetGlobalVar("pathAler", @"JabeChatCommands\Alerts\");
+		CPH.SetGlobalVar("pathRede", @"JabeChatCommands\Redeems\");
+		CPH.SetGlobalVar("pathTime", @"JabeChatCommands\Timers\");
 		
+		// OBS stuff
 		CPH.SetGlobalVar("obsSceneEffects", "Component Overlay Effects");
 		CPH.SetGlobalVar("obsSourceSFX", "SFX");
 		CPH.SetGlobalVar("obsSourceGFX", "GFX");
 		CPH.SetGlobalVar("obsSourceEmbed", "Embed");
 	
-	        // Resets the first and the roulette
-	        CPH.SetGlobalVar("first", null);
-	        CPH.SetGlobalVar("chanceRoulette", 6);
-	        CPH.SetGlobalVar("collabLink", "");
-	        //CPH.SetGlobalVar("currentVip", File.ReadAllText(""));
+        // Resets the stream specific variables
+        CPH.SetGlobalVar("first", null);
+        CPH.SetGlobalVar("chanceRoulette", 5);
+        CPH.SetGlobalVar("collabLink", null);
+		CPH.SetGlobalVar("usersThatSaidSomething", new List<string>());
+		CPH.SetGlobalVar("currentViewerCount", 0);
 
 		return true;
 	}
